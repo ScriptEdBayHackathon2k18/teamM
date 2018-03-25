0d6a6cd6463ea1d20a403b3e85b77f3cf8bf4d57
Glitch (responsible-daffodil) <none>
2018-03-25 22:56:58 +0000

    🐟⛲️ Checkpoint ./public/data.json:789123/3712 ./public/client.js:789225/195 ./views/story.html:789123/2

--- public/client.js
+++ public/client.js
@@ -31,24 +31,24 @@ $(function() {
   function handleButtonClick(){
     var screenId;
     screenId = $(this).data("next-screen");
-    console.log(data[screenId]);
     setUpPage(data[screenId]);
   };
   
   function setUpPage(screen){
     var content = screen.content;
     $("#content").html(content);
-    console.log("hey");
-    $("#leftbutton").data("next-screen", screen.options.leftbutton.link_id);
-    $("#leftbutton").text(screen.options.leftbutton.text);
-    console.log("no");
+    $("body").css("background-color", screen.background); 
+    
+    $("#leftButton").data("next-screen", screen.options.leftbutton.link_id);
+    $("#leftButton").text(screen.options.leftbutton.text);
+    
     if(screen.options.middlebutton){
-      $("#middlebutton").data("next-screen", screen.options.middlebutton.link_id);
-      $("#middlebutton").text(screen.options.middlebutton.text);
+      $("#middleButton").data("next-screen", screen.options.middlebutton.link_id);
+      $("#middleButton").text(screen.options.middlebutton.text);
     }
     if(screen.options.rightbutton){    
-        $("#rightbutton").data("next-screen", screen.options.rightbutton.link_id);
-        $("#rightbutton").text(screen.options.rightbutton.text);
+        $("#rightButton").data("next-screen", screen.options.rightbutton.link_id);
+        $("#rightButton").text(screen.options.rightbutton.text);
     }
   };
   
