5e6dd273e7d40ba200392025bb73506504ac8ea8
Glitch (responsible-daffodil) <none>
2018-03-25 19:07:17 +0000

    🍍🍡 Checkpoint ./public/data.json:789206/28 ./public/client.js:789118/8:789123/8 ./views/index.html:789118/150:789123/1006:789225/88 ./public/data.js:789118/98:789206/1

--- public/data.js
+++ public/data.js
@@ -1,15 +1,17 @@
-{
+var data={
   "id": 1,
-  "content": '<p>Doctor: "Are the wires connectedou ready to complete said incision?"</p><p>You: "Yes. His life depends on me doesn\'t it?"
-        </p>
+  "content": 
+        <p>Doctor: "Are the wires connectedou ready to complete said incision?"</p>
+        <p>You: "Yes. His life depends on me doesn't it?"</p>
         <p>
         Doctor: "It does. Your decisions will be a great impact in 
-        your friend\'s mental state. You will be launched into his
+        your friend's mental state. You will be launched into his
         consciousness in 3... 2... 1..."
         </p>        
         <p id="subtext"> 
         You are in. Which memory will you alter first?  
         </p>',
+  "background" : "#808080",
   "options": {
     "1": {
       "text": "the blue sky",
