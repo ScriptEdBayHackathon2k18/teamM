98d9280a77c4449320b2a448b4ac170e930926ce
Glitch (responsible-daffodil) <none>
2018-03-25 18:56:56 +0000

    💒🎪 Checkpoint ./public/data.json:789118/2:789174/19:789206/2232:789225/9 ./views/index.html:789118/7:789123/126:789225/11 ./task.md:789225/16 ./public/client.js:789118/448:789123/43:789225/4 ./public/style.css:789118/53

--- public/client.js
+++ public/client.js
@@ -1,4 +1,15 @@
 $(function() {
   console.log('hello world :o')
 })
-$ #
+$("#option1").click(function() {
+  $("body").css("background-color", "#888888");
+})
+//option 1 makes it brighter
+$("#option2").click(function() {
+  $("body").css("background-color", "#787878");
+})
+//option 2 makes it darker
+$("#option3").click(function() {
+  $("body").css("background-color", "#909090");
+})
+//option 3 makes it brighter
--- task.md
+++ task.md
@@ -6,6 +6,7 @@ Todo
   - [Bryan] Make a click handler funciton that changes the background color 
   - [Farrah] Make an understandable story with logical branches
   - [] Change content based on the button clicked
+  -
 Done
 ---
    
