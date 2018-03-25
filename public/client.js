d5d8e781ec70d92140a0eb2a23cfebcd7f19202f
Glitch (responsible-daffodil) <none>
2018-03-25 19:39:16 +0000

    🚇💄 Checkpoint ./views/index.html:789123/456:789206/34 ./public/client.js:789123/55:789206/664:789225/3 ./task.md:789225/5 ./public/data.json:789206/472 ./public/style.css:789225/3

--- public/client.js
+++ public/client.js
@@ -3,19 +3,29 @@
 })
 $("#optionone").click(function() {
   $("body").css("background-color", "#888888");
-  
+  $("body").text("#sctwobs");
 })
 //option 1 makes it brighter
 $("#optiontwo").click(function() {
   $("body").css("background-color", "#787878");
+  $("body"
 })
 //option 2 makes it darker
 var success = function(data) {
   console.log("here is the data", data);
 }
 var fetchData = function(){
-  $.getJSON("/data.json", function(result) {
-    console.log("getjson handler");
-    console.log(result);
+  $.getJSON("/data.json", function() {
+  console.log( "success" );
+})
+  .done(function() {
+    console.log( "second success" );
+  })
+  .fail(function(error) {
+    console.log( "error" );
+    console.log(error);
+  })
+  .always(function() {
+    console.log( "complete" );
   });
 }
--- task.md
+++ task.md
@@ -3,7 +3,6 @@ Tasks
    
 Todo
 ---
-  - 
   - [Farrah] Make an understandable story with logical branches
   - [] Change content based on the button clicked
   -
