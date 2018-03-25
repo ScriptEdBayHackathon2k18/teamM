0636f5fa3a55778a2a059a2b6abbed5cc68526c2
Glitch (responsible-daffodil) <none>
2018-03-25 22:25:25 +0000

    🎽🏄 Checkpoint ./views/story.html:789123/575 ./public/client.js:789225/757 ./public/data.json:789123/498

--- public/client.js
+++ public/client.js
@@ -1,35 +1,43 @@
-
 $(function() {
+  var data;
+  function setData(data) {
+    data = data;
+  }
+  function fetchData(){
+    $.getJSON("/dev.json", function(data) {
+      console.log( "success" );
+      setData(data);
+    })
+    .done(function() {
+      console.log( "second success" );
+    })
+    .fail(function(error) {
+      console.log( "error" );
+      console.log(error);
+    })
+    .always(function() {
+      console.log( "complete" );
+    });
+  }
   
-  var fetchData = function(){
-  $.getJSON("/dev.json", function(data) {
+  var data = fetchData(); 
+  console.log(data);
+  
+  $("#leftButton").click(function() {
     console.log(data);
-    console.log( "success" );
-  })
-  .done(function() {
-    console.log( "second success" );
-  })
-  .fail(function(error) {
-    console.log( "error" );
-    console.log(error);
-  })
-  .always(function() {
-    console.log( "complete" );
+    handleButtonClick
   });
-  } 
-  window.data= fetchData(); 
-  $("#leftButton").click(handleButtonClick);
   $("#middleButton").click(handleButtonClick);
   $("#rightButton").click(handleButtonClick);
   
-    function handleButtonClick(){
+  function handleButtonClick(){
     var screenId;
     screenId = $(this).data("next-screen");
     setUpPage(screenId)
   };
   
-  function setUpPage(id){
-    var content = window.data[id][content];
+  function setUpPage(data, id){
+    var content = data[id][content];
     console.log(content);
   };
   
